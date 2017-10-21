bbc9f6123c9a1aea93c59303834697204cdec380
Craig Steele <csteele23@Craigs-MacBook-Pro.local>
2017-10-21 18:35:34 -0400

    changed cors config

--- src/main/java/com/craig/cards/config/SimpleCORSFilter.java
+++ src/main/java/com/craig/cards/config/SimpleCORSFilter.java
@@ -29,11 +29,11 @@ public class SimpleCORSFilter implements Filter{
 		HttpServletRequest request = (HttpServletRequest) req;
 	    HttpServletResponse response = (HttpServletResponse) res;
 
-	    response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+	    response.setHeader("Access-Control-Allow-Origin", "*");
 	    response.setHeader("Access-Control-Allow-Credentials", "true");
-	    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+	    response.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
 	    response.setHeader("Access-Control-Max-Age", "3600");
-	    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+	    response.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json");
 
 	    chain.doFilter(req, res);
 		
