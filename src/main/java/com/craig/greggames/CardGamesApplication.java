5ab96dc0aed25c6c3b16501090a5e2746806ff7c
Craig Steele <csteele23@Craigs-MacBook-Pro.local>
2017-11-23 15:06:16 -0500

    commented out zuul

--- src/main/java/com/craig/greggames/CardGamesApplication.java
+++ src/main/java/com/craig/greggames/CardGamesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
+//@EnableZuulProxy
 public class CardGamesApplication {
 
 	public static void main(String[] args) {
